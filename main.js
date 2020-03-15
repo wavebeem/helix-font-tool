7d64fc7256931ed78d3e04f746dd76e30f98d71f
Brian Mock <brian@mockbrian.com>
2020-03-14 23:19:58 -0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,201 @@
+const FONT_WIDTH = 6;
+const FONT_HEIGHT = 8;
+
+function main() {
+  customElements.define("helix-files", HelixFilesElement);
+  customElements.define("helix-preview", HelixPreviewElement);
+  customElements.define("helix-font", HelixFontElement);
+}
+
+function getTemplate(selector) {
+  const template = document.querySelector(selector);
+  return document.importNode(template.content, true);
+}
+
+class HelixFilesElement extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = "";
+    this.appendChild(getTemplate("#template-helix-files-element"));
+    this.input = this.querySelector("input");
+  }
+}
+
+class HelixPreviewElement extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = "";
+    this.classList.add("helix-preview");
+    this.appendChild(getTemplate("#template-helix-preview-element"));
+    this.hCanvas = this.querySelector("[data-orientation='horizontal']");
+    this.vCanvas = this.querySelector("[data-orientation='vertical']");
+    // this.longSize = 192;
+    // this.shortSize = 192;
+    this.longSize = 192;
+    this.shortSize = 64;
+    this.scale = 3;
+
+    this.hCanvas.width = this.longSize;
+    this.hCanvas.height = this.shortSize;
+    this.hCanvas.style.width = this.longSize * this.scale + "px";
+    this.hCanvas.style.height = this.shortSize * this.scale + "px";
+    this.hCanvas.style.imageRendering = "crisp-edges";
+    this.hCanvas.style.imageRendering = "pixelated";
+    this.hCtx = this.hCanvas.getContext("2d");
+
+    this.vCanvas.width = this.shortSize;
+    this.vCanvas.height = this.longSize;
+    this.vCanvas.style.width = this.shortSize * this.scale + "px";
+    this.vCanvas.style.height = this.longSize * this.scale + "px";
+    this.vCanvas.style.imageRendering = "crisp-edges";
+    this.vCanvas.style.imageRendering = "pixelated";
+    this.vCtx = this.vCanvas.getContext("2d");
+
+    this.img = document.createElement("img");
+    this.img.onload = () => {
+      this._update();
+    };
+    this.img.src = "font5x7.png";
+    // ---[ HACK ]--------
+    Object.assign(window, {
+      vCtx: this.vCtx,
+      hCtx: this.hCtx,
+      img: this.img
+    });
+  }
+
+  get file0() {
+    return this._file0;
+  }
+
+  set file0(file0) {
+    this._file0 = file0;
+    this._update();
+  }
+
+  get file1() {
+    return this._file1;
+  }
+
+  set file1(file1) {
+    this._file1 = file1;
+    this._update();
+  }
+
+  _update() {
+    console.log({
+      vCtx: this.vCtx,
+      hCtx: this.hCtx,
+      file0: this.file0,
+      file1: this.file1,
+      img: this.img
+    });
+
+    this.hCtx.fillStyle = "var(--bit-color0)";
+    this.hCtx.fillRect(0, 0, this.hCanvas.width, this.hCanvas.height);
+    this.hCtx.drawImage(this.img, 0, 0);
+
+    this.vCtx.fillStyle = "var(--bit-color0)";
+    this.vCtx.fillRect(0, 0, this.vCanvas.width, this.vCanvas.height);
+    this._drawRotated(Math.PI / 2);
+    const imageData = this.vCtx.getImageData(
+      0,
+      0,
+      this.vCanvas.width,
+      this.vCanvas.height
+    );
+    const event = new CustomEvent("update", { detail: { imageData } });
+    this.dispatchEvent(event);
+  }
+
+  _drawRotated(radians) {
+    const { width, height } = this.vCanvas;
+    this.vCtx.save();
+    this.vCtx.translate(width / 2, height / 2);
+    this.vCtx.rotate(radians);
+    this.vCtx.drawImage(this.img, -height / 2, -width / 2);
+    this.vCtx.restore();
+  }
+}
+
+class HelixFontElement extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = "";
+    this.appendChild(getTemplate("#template-helix-font-element"));
+    document.querySelector("#preview").addEventListener("update", event => {
+      this._update(event.detail.imageData);
+    });
+  }
+
+  // for colc in range(colmax):
+  //   for rowc in range(rowmax):
+  //     line = ""
+  //     for fontwc in range(fontw):
+  //       val=0
+  //       for fonthc in range(fonth):
+  //         b = 1 if im[fontw*rowc+fontwc][fonth*(colmax-1)-fonth*colc+fonthc][0]<128 else 0
+  //         val = (val<<1)|b
+  //       line += '0x%02X' % val + ', '
+  //     print (line)
+  _update(imageData) {
+    const { height } = imageData;
+    const array = imageDataToArray(imageData);
+    console.log(array);
+    const lines = [];
+    const COL_MAX = 32;
+    const ROW_MAX = 8;
+    for (let col = 0; col < COL_MAX; col++) {
+      for (let row = 0; row < ROW_MAX; row++) {
+        const line = [];
+        for (let fontW = 0; fontW < FONT_WIDTH; fontW++) {
+          let curByte = 0;
+          for (let fontH = 0; fontH < FONT_HEIGHT; fontH++) {
+            // [fontw*rowc+fontwc][fonth*(colmax-1)-fonth*colc+fonthc]
+            const i = FONT_WIDTH * row + fontW;
+            const j = FONT_HEIGHT * (height - 1) - FONT_HEIGHT * col + fontH;
+            const bit = array[i][j];
+            curByte = (curByte << 1) | bit;
+          }
+          line.push(formatByte(curByte));
+        }
+        // console.log(line.join(", "));
+        lines.push(line.join(", "));
+      }
+    }
+    const code = lines.join("\n") + "\n";
+    console.log(code);
+    this.querySelector("textarea").value = code;
+  }
+}
+
+function formatByte(b) {
+  console.log(b);
+  return "0x" + b.toString(16).padStart(2, "0");
+}
+
+function imageDataToArray(imageData) {
+  const { data, width, height } = imageData;
+  const bpp = 4;
+  const threshold = 128;
+  const array = [];
+  for (let y = 0; y < height; y++) {
+    const row = [];
+    for (let x = 0; x < width; x++) {
+      const i = (y * width + x) * bpp;
+      const red = data[i];
+      row.push(red > threshold ? 1 : 0);
+    }
+    array.push(row);
+  }
+  return array;
+}
+
+// ---[ HACK ]---------
+Object.assign(window, {
+  imageDataToArray
+});
+
+main();
+
+// <helix-file id="file1"></file-uploader>
+// <helix-file id="file2"></file-uploader>
+// <helix-preview></helix-preview>
+// <helix-font></helix-font>
